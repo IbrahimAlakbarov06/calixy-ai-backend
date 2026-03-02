c0dd9f7cd7495e77fb2663a1b57916900ff02b26
IbrahimAlakbarov06 <ibrahim.alakbarov2006@gmail.com>
2026-03-02 18:48:07 +0400

    creating project

---
+++ src/test/java/calixy/CalixyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package calixy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalixyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
