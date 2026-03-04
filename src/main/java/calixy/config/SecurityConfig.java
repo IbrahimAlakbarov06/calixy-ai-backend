e2817f9f2f9fe45eef59f9e394d8398149446ab8
IbrahimAlakbarov06 <ibrahim.alakbarov2006@gmail.com>
2026-03-04 20:02:19 +0400

    fixing annotation

--- src/main/java/calixy/config/SecurityConfig.java
+++ src/main/java/calixy/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package calixy.config;
 
 import calixy.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,7 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
-@Component
+@Configuration
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
