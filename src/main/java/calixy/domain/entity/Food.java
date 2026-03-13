8095090d0e6beb0c19213766c5aaf83259076836
IbrahimAlakbarov06 <ibrahim.alakbarov2006@gmail.com>
2026-03-13 21:09:40 +0400

    add new entity

---
+++ src/main/java/calixy/domain/entity/Food.java
@@ -0,0 +1,68 @@
+package calixy.domain.entity;
+
+import calixy.model.enums.FoodCategory;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "foods")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Food {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "name_az")
+    private String nameAz;
+
+    @Column(nullable = false)
+    private Double caloriesPer100g;
+
+    @Column(nullable = false)
+    private Double proteinPer100g;
+
+    @Column(nullable = false)
+    private Double carbsPer100g;
+
+    @Column(nullable = false)
+    private Double fatPer100g;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private FoodCategory category;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Boolean isAzerbaijani = false;
+
+    private String imageUrl;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Boolean isActive = true;
+
+    @OneToMany(mappedBy = "food", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @Builder.Default
+    private List<MealLog> mealLogs = new ArrayList<>();
+
+    @OneToMany(mappedBy = "food", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @Builder.Default
+    private List<FavoriteFood> favoriteFoods = new ArrayList<>();
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
